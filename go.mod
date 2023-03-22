a060b65ed43be5f998581f25fe2c4a760ea9a900
Nebojsa Janosevic <njanosevic@control4.com>
2023-03-22 12:26:33 +0100

    set and put minio

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/njanosevic/pkg-resource
 
 go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/minio/minio-go/v7 v7.0.49
+)
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
--- minio/minio.go
+++ minio/minio.go
@@ -1,10 +1,76 @@
 package minio
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	_ "github.com/joho/godotenv/autoload" // load .env file automatically
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"log"
+	"strconv"
+)
+
+type SetMinio struct {
+	Endpoint        string
+	AccessKeyID     string
+	SecretAccessKey string
+	Location        string
+	UseSSL          string
+}
+
+type PutFile struct {
+	Bucket      string
+	Name        string
+	Path        string
+	ContentType string
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+
+func setMinio(setMinio SetMinio) (*minio.Client, error) {
+	useSSL, errSsl := strconv.ParseBool(setMinio.UseSSL)
+	if errSsl != nil {
+		log.Println(errSsl)
+		return nil, nil
+	}
+
+	// Initialize minio client object.
+	minioClient, errInit := minio.New(setMinio.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(setMinio.AccessKeyID, setMinio.SecretAccessKey, ""),
+		Secure: useSSL,
+	})
+	return minioClient, errInit
+}
+
+func minioPutFile(minioClient *minio.Client, putFile PutFile, setMinio SetMinio) {
+	ctx := context.Background()
+
+	// Make a new bucket called dev-minio.
+	err := minioClient.MakeBucket(ctx, putFile.Bucket, minio.MakeBucketOptions{Region: setMinio.Location})
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(ctx, putFile.Bucket)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", putFile.Bucket)
+		} else {
+			log.Printf(err.Error())
+			return
+		}
+	} else {
+		log.Printf("Successfully created %s\n", putFile.Bucket)
+	}
+
+	// Upload the zip file with FPutObject
+	info, err := minioClient.FPutObject(ctx, putFile.Bucket, putFile.Name, putFile.Path, minio.PutObjectOptions{ContentType: putFile.ContentType})
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", putFile.Name, info.Size)
 }
